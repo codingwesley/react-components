2206d095d2a18c5b0525486c288b6954e317a34e
Wesley.CHEN <codingwelsey@163.com>
2021-12-05 12:08:52 +0800

    cypress with storybook init

---
+++ src/tests/index.spec.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { composeStories } from "@storybook/testing-react";
+import { mount } from "@cypress/react";
+import * as stories from "../stories/Button.stories";
+
+// compile the "Primary" story with the library
+const { Primary } = composeStories(stories);
+
+it("button component test example", () => {
+  mount(<Primary />);
+
+  cy.contains("Button").should("exist");
+});
